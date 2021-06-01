15e26f9efa08dbe6aab354b663a2e667383b1e11
HarishJha21 <84332406+HarishJha21@users.noreply.github.com>
2021-06-01 12:29:21 +0530

    Add files via upload

---
+++ literals.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class literals {
+    public static void main(String[] args) {
+        byte age = 34;
+        int age2 = 57;
+        short age3 = 87;
+        long ageDino = 58899776535797L;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d1 = 4.66D;
+        boolean a = true;
+        System.out.println(age);
+        String str = "Harish";
+        System.out.println(str);
+
+    }
+}
+    
+
