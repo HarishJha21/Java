8874ae1c575e1304f0735099df17d9571e2807dd
HarishJha21 <84332406+HarishJha21@users.noreply.github.com>
2021-06-04 14:33:07 +0530

    Add files via upload

---
+++ Practice1.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Practice1 {
+    public static void main(String[] args) {
+        // Question 1
+        // int a = 34;
+        // int b = 7;
+        // int c = 4;
+        // int sum =a + b+c;
+        // System.out.println(sum);
+        
+        // Question 2
+        // float subject1 = 47;
+        // float subject2 = 77;
+        // float subject3 = 93;
+        // float cgpa = (subject1 + subject2 + subject3)/30;
+        // System.out.println(cgpa);
+
+        // Question 3
+        // System.out.println("What is your name?");
+        // Scanner sc = new Scanner(System.in);
+        // String name = sc.next();
+        // System.out.println("Hello " + name + " have a perfect day ahead!");
+
+        // Question 4
+        System.out.println("Enter your number");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.hasNextInt());
+           
+        }
+}
+        
