a24005204a6d5187573cbb2c2148b1b46d57cf62
HarishJha21 <84332406+HarishJha21@users.noreply.github.com>
2021-06-05 17:57:14 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,29 @@
+public class Operators {
+    public static void main(String[] args) {
+        // 1. Arithmetic Operators
+        int a = 6;
+        // int b = 9 % a; //Modulo Operator
+        // 4.8%1.2 --> Returns Decimal Reminder
+
+
+        // 2. Assignment Operators
+        int b =7;
+        b *=4;
+        System.out.println(b); 
+        
+        // 3. Comparision Operators
+        // System.out.println(74<7);
+        
+        // 4. Logical Operators
+        // System.out.println(65>9 && 54>78);
+        System.out.println(65>9 || 54>78);
+
+        // 5. Bitwise Operators
+        System.out.println(2&3);
+        //12
+        //13
+        //-------
+        //12
+
+    }
+}
