0e0ad7eb2d349b8564c4e50dc27cf7fc9e957bbf
HarishJha21 <84332406+HarishJha21@users.noreply.github.com>
2021-06-08 12:37:51 +0530

    Add files via upload

---
+++ Resulting.java
@@ -0,0 +1,24 @@
+public class Resulting {
+    public static void main(String[] args) {
+        /* byte x = 7;
+        int y = 8;
+        short z = 9;
+        int a = y + z;
+        float b = 6.76f + x;
+        System.out.println(b); */
+
+        //Increment and Decrement Operators
+        int i = 57;
+        //int b = i++; // first b is assigned in i (57) and then i is incremented
+        int j = 68;
+        int c = ++j; // first j is incremented then c is assigned in j
+        System.out.println(i++);
+        System.out.println(i);
+        System.out.println(++i);
+        System.out.println(i);
+        int y = 7;
+        System.out.println(++y *8);
+        char ch = 'a';
+        System.out.println(++ch);    
+    }
+}
