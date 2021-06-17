81e5e6d1a6d3ebedc7cc7e4701496d4ecd6e036e
HarishJha21 <84332406+HarishJha21@users.noreply.github.com>
2021-06-17 12:38:38 +0530

    Add files via upload

---
+++ conditions.java
@@ -0,0 +1,14 @@
+public class conditions {
+    public static void main(String[] args) {
+        int age = 19;
+        boolean condition = (age==18);
+
+        if(condition){
+            System.out.println("Haa dost aap gaadi chala sakte hai!");
+        }
+        else{
+            System.out.println("Nahi dost aap gaadi nahi chala sakte hai!");
+        }
+    }
+}
+
