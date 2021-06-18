a8d7746020371231cf14ecfab0db4c4f592a7e4f
HarishJha21 <84332406+HarishJha21@users.noreply.github.com>
2021-06-18 12:33:11 +0530

    Add files via upload

---
+++ logical.java
@@ -0,0 +1,29 @@
+public class logical {
+    public static void main(String[] args) {
+        System.out.println("For logical AND...");
+        boolean a = true;
+        boolean b = true;
+        // if (a && b){
+        //     System.out.println("Y");
+        // }
+        // else{
+        //     System.out.println("N");
+        // }
+
+            System.out.println("For logical OR...");
+            
+            // if (a || b){
+            //     System.out.println("Y");
+            // }
+            // else{
+            //     System.out.println("N");
+            // }
+
+            System.out.println("For Logical NOT...");
+            System.out.print("Not(a) is ");
+            System.out.println(!a);
+            System.out.print("Not(b) is ");
+            System.out.println(!b);
+
+        }
+    }
