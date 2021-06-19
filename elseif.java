356b1eeb3345d6025f62777ed5feda851dc1f5b6
HarishJha21 <84332406+HarishJha21@users.noreply.github.com>
2021-06-19 11:19:52 +0530

    Add files via upload

---
+++ elseif.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class elseif {
+    public static void main(String[] args) {
+        String var = "Harish";
+        
+        switch(var) {
+            case "Sanika":
+                System.out.println("You are mature legally now!");
+                break;
+            case "Rishika":
+                System.out.println("You can marry now!");
+                break;
+            case "Kashish":
+                System.out.println("You going to chill now!");
+                break;
+            default:
+                System.out.println("Jiyo jee bharke!");
+        }
+        System.out.println("Thanks for viewing my Code");
+
+
+        // int age;
+        // System.out.println("Enter Your Age");
+        // Scanner sc = new Scanner(System.in);
+        // age = sc.nextInt();
+        // if (age>69){
+        //     System.out.println("Gyaan hai aapko!");
+        // }
+        // else if(age>59){
+        //     System.out.println("Aadha Gyaan hai aapko!");
+        // }
+        // else if(age>49){
+        //     System.out.println("Aadha se thoda kaam Gyaan hai aapko!");
+        // }
+        // else{
+        //     System.out.println("Gyaan nahi hai aapko!");
+        // }
+        // if(age>3){
+        //     System.out.println("You are not a baby anymore!")
+        // }
+    } 
+}
