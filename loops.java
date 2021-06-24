bdaf3faf0a683d2924de0dc974ef7e9aeeb6de81
HarishJha21 <84332406+HarishJha21@users.noreply.github.com>
2021-06-24 12:31:17 +0530

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,15 @@
+public class loops {
+    public static void main(String[] args) {
+        System.out.println("Using Loops:");
+        int i = 100;
+        while(i<=200){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Khatam ho gaya bhai!");
+
+        // while(true){
+        //     Sys.out.println("I am a infinite while loop!");tem
+        // }
+    }    
+}
