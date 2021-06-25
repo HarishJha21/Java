49a7fca4b54db465130171c2eaae13dd8c42f3d1
HarishJha21 <84332406+HarishJha21@users.noreply.github.com>
2021-06-25 12:09:56 +0530

    Add files via upload

---
+++ Do while.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Do while {
+    public static void main(String[] args) {
+    //    int a = 0;
+    //    while(a<6){
+    //        System.out.println(a);
+    //        a++;
+    //    }
+
+        int b = 9;
+        do {
+            System.out.println(b);
+            b++;
+        }while(b<5);
+
+        int c = 2;
+        do{
+            System.out.println(c);
+            c++;
+        }while(c<=21);
+    }
+}
